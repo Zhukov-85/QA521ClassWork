d606cd3ede192cc039e29acb2209a04fbc70afb9
Dmitriy Sulzhits <dsulzhits@gmail.com>
2025-09-06 15:55:30 +0300

    Практика урок 2

---
+++ lesson_02/lesson_02_14_practice.py
@@ -0,0 +1,13 @@
+# вариант 1
+side_a = input("Введите ширину: ")
+side_b = input("Введите высоту: ")
+side_a_float = float(side_a)
+side_b_float = float(side_b)
+
+print("Площадь: ", side_a_float * side_b_float)
+
+# вариант 2
+side_a = float(input("Введите ширину: "))
+side_b = float(input("Введите высоту: "))
+
+print("Площадь: ", side_a * side_b)
